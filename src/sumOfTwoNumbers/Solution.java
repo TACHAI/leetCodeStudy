f45877ccc47a80c0557c8632e96b872954e618c7
tachai <1206966083@qq.com>
2021-07-26 21:42:38 +0800

    添加题目描述

--- src/sumOfTwoNumbers/Solution.java
+++ src/sumOfTwoNumbers/Solution.java
@@ -4,6 +4,17 @@ package sumOfTwoNumbers;
  * Create by tachai on 2021/7/26 7:45 下午
  * gitHub https://github.com/TACHAI
  * Email tc1206966083@gmail.com
+ *
+ * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  */
 public class Solution {
 
