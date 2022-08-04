25b2f8f8eb138f1394133145be3dec8c607bb4ee
tachai <1206966083@qq.com>
2022-08-04 21:11:26 +0800

    第62题  不同路径  自己写的递归方法

--- src/uniquePaths/Solution.java
+++ src/uniquePaths/Solution.java
@@ -10,7 +10,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * https://leetcode.cn/problems/unique-paths/
  *
  *
- * 自己写的递归  网站上不人  emco
+ * 自己写的递归  网站上不能通过 本地是可以的  emco
  */
 public class Solution {
 
@@ -36,8 +36,27 @@ public class Solution {
         solation(m-1,n);
         solation(m,n-1);
     }
+     // 网站上推荐的dp
+    public int method2(int m,int n){
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            f[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            f[0][j] = 1;
+        }
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
 
     public static void main(String[] args) {
         System.out.println(uniquePaths(19,13));
+
+
+
     }
 }
